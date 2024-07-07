b6bd64754cd7dfc01e8b24004faff24078a86eb3
Matt Gordon <matt@mattrgordon.com>
2024-07-07 17:38:53 -0500

    added triangle test

--- tests/createSVG.test.js
+++ tests/createSVG.test.js
@@ -1,10 +1,13 @@
-// const CreateSVG = require('../index.js');
+const Triangle = require('../lib/triangle');
 
 describe('Create an SVG', () => {
-  describe('add characters', () => {
-    it('should take in only 3 characters', () => {
-      const characters = 'abc';
-      expect(characters.length).toBeLessThanOrEqual(3);
+  describe('create shape', () => {
+    it('should be a blue triangle', () => {
+      const shape = new Triangle();
+      shape.setColor('blue');
+      expect(shape.render()).toEqual(
+        '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
+      );
     });
   });
 });
