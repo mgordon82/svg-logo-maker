4ff69bdae969bbd36ffce83ab0dc824fc9430798
Matt Gordon <matt@mattrgordon.com>
2024-07-09 19:57:27 -0500

    first attempt at creating a test
    ps it failed

--- tests/createSVG.test.js
+++ tests/createSVG.test.js
@@ -3,8 +3,7 @@ const Triangle = require('../lib/triangle');
 describe('Create an SVG', () => {
   describe('create shape', () => {
     it('should be a blue triangle', () => {
-      const shape = new Triangle();
-      shape.setColor('blue');
+      const shape = new Triangle('', '', 'blue');
       expect(shape.render()).toEqual(
         '<polygon points="150, 18 244, 182 56, 182" fill="blue" />'
       );
